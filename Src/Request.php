36029d1f11c502b958eea754cea5a042d8875f77
Alison Vitor <71190247+Alison561@users.noreply.github.com>
2020-10-15 21:01:57 -0300

    1.1

--- Src/Request.php
+++ Src/Request.php
@@ -45,7 +45,7 @@ class Request
         }
     }
  
-    protected function sefFiles() {
+    protected function setFiles() {
         foreach ($_FILES as $key => $value) {
             $this->files[$key] = $value;
         }
